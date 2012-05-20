e80bd6d4054a6524993d45de8872fed2237cfff5
Bryan Bibat <bry@bryanbibat.net>
2012-05-20 23:59:39 +0800

    remove txt file from packaged tar

--- bin/package.sh
+++ bin/package.sh
@@ -2,6 +2,6 @@
 
 cd ..
 rm dev-challenge.tar.gz
-tar --exclude=.gitignore --exclude=bin --exclude=spec --exclude=*.git --exclude=*.csv --exclude=node_modules --exclude=*.swp --exclude=.gitignore -zcvf dev-challenge.tar.gz dev-challenge/
+tar --exclude=.gitignore --exclude=bin --exclude=spec --exclude=*.txt --exclude=*.git --exclude=*.csv --exclude=node_modules --exclude=*.swp --exclude=.gitignore -zcvf dev-challenge.tar.gz dev-challenge/
 md5sum dev-challenge.tar.gz
 cd dev-challenge
